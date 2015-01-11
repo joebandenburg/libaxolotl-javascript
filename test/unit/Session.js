e57d41ef59f30fa215bac4c37253d7f2b19280f2
Joe Bandenburg <joe@bandenburg.com>
2015-01-11 16:45:16 +0000

    Add test for message/session version mismatch
    Also removed some old useless tests

--- test/unit/Session.js
+++ test/unit/Session.js
@@ -1,60 +1,31 @@
 import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
 import Session from "../../src/Session";
+import SessionState from "../../src/SessionState";
+import Messages from "../../src/Messages";
+import {InvalidMessageException} from "../../src/Exceptions";
 import crypto from "./FakeCrypto";
 import co from "co";
 
+chai.use(chaiAsPromised);
 var assert = chai.assert;
 
 describe("Session", () => {
-    describe("encryptMessage", () => {
-        it("WhisperMessage", co.wrap(function*() {
-            var state = {
-                sessionVersion: 3,
-                sendingChain: {
-                    key: new ArrayBuffer(32),
-                    index: 0
-                },
-                senderRatchetKeyPair: {
-                    public: new ArrayBuffer(32),
-                    private: new ArrayBuffer(32)
-                },
-                previousCounter: 0,
-                localIdentityKey: new ArrayBuffer(32),
-                remoteIdentityKey: new ArrayBuffer(32)
-            };
+    describe("decryptMessage", () => {
+        it("rejects message with wrong version number", co.wrap(function*() {
+            var state = new SessionState({
+                sessionVersion: 3
+            });
             var session = new Session(crypto, state);
-            var message = yield session.encryptMessage(new Uint8Array([1, 2, 3]).buffer);
-            //console.log(message);
-            //assert.equal(message, new Uint8Array([51, 10, 32]).buffer);
-            assert.equal(state.sendingChain.index, 1);
-        }));
-        it("PreKeyWhisperMessage", co.wrap(function*() {
-            var state = {
-                sessionVersion: 3,
-                sendingChain: {
-                    key: new ArrayBuffer(32),
-                    index: 0
+            var message = Messages.encodeWhisperMessage({
+                version: {
+                    current: 2,
+                    max: 3
                 },
-                senderRatchetKeyPair: {
-                    public: new ArrayBuffer(32),
-                    private: new ArrayBuffer(32)
-                },
-                senderCounter: 0,
-                previousCounter: 0,
-                localIdentityKey: new ArrayBuffer(32),
-                remoteIdentityKey: new ArrayBuffer(32),
-                pendingPreKey: {
-                    preKeyId: 1,
-                    signedPreKeyId: 1,
-                    baseKey: new ArrayBuffer(32)
-                },
-                localRegistrationId: 10
-            };
-            var session = new Session(crypto, state);
-            var message = yield session.encryptMessage(new Uint8Array([1, 2, 3]).buffer);
-            //console.log(message);
-            //assert.equal(message, new Uint8Array([51, 10, 32]).buffer);
-            assert.equal(state.sendingChain.index, 1);
+                message: {},
+                mac: new ArrayBuffer(8)
+            });
+            yield assert.isRejected(session.decryptMessage(message), InvalidMessageException);
         }));
     });
 });
