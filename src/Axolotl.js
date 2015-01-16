7527cc644ced32e8a119b6397b4366ef21572bc9
Joe Bandenburg <joe@bandenburg.com>
2015-01-16 16:54:20 +0000

    Add JSDoc to Axolotl.js

--- src/Axolotl.js
+++ src/Axolotl.js
@@ -22,6 +22,69 @@ import Store from "./Store";
 import Crypto from "./Crypto";
 import co from "co";
 
+/**
+ * A public/private key pair
+ * @typedef {object} KeyPair
+ * @property {ArrayBuffer} public - the public key bytes
+ * @property {ArrayBuffer} private - the private key bytes
+ */
+
+/**
+ * @typedef {object} PreKey
+ * @property {number} id - a 16-bit identifier for the signed pre key
+ * @property {KeyPair} keyPair - a key pair
+ */
+
+/**
+ * A {@link KeyPair} with a signature.
+ * @typedef {object} SignedPreKey
+ * @property {number} id - a 16-bit identifier for the signed pre key
+ * @property {KeyPair} keyPair - a key pair
+ * @property {ArrayBuffer} signature - a signature for the key pair
+ */
+
+/**
+ * A service providing cryptographic primitives. See the README for details on the methods.
+ * @typedef {object} Crypto
+ * @property {function} generateKeyPair
+ * @property {function} calculateAgreement
+ * @property {function} randomBytes
+ * @property {function} sign
+ * @property {function} verifySignature
+ * @property {function} hmac
+ * @property {function} encrypt
+ * @property {function} decrypt
+ */
+
+/**
+ * A service providing various storage facilities. See the README for details on the methods.
+ * @typedef {object} Store
+ * @property {function} getLocalIdentityKeyPair
+ * @property {function} getLocalRegistrationId
+ * @property {function} getLocalSignedPreKeyPair
+ * @property {function} getLocalPreKeyPair
+ * @property {function} getRemotePreKeyBundle
+ * @property {function} isRemoteIdentityTrusted
+ * @property {function} putRemoteIdentity
+ * @property {function} hasSession
+ * @property {function} getSession
+ * @property {function} putSession
+ */
+
+/**
+ * An unique identifier for a remote entity. Clients are free to use whatever they wish for this.
+ * @typedef {*} Identity
+ */
+
+/**
+ * A single Axolotl instance may be used to encrypt/decrypt messages to/from many remote entities.
+ * <p>
+ * Clients must implement and supply both the crypto and store services, which are required by Axolotl to function.
+ *
+ * @param {Crypto} crypto - cryptographic service
+ * @param {Store} store - storage service
+ * @constructor
+ */
 function Axolotl(crypto, store) {
     var self = this;
 
@@ -33,13 +96,15 @@ function Axolotl(crypto, store) {
     /**
      * Generate an identity key pair. Clients should only do this once, at install time.
      *
-     * @return {KeyPair} generated key pair.
+     * @method
+     * @return {Promise.<KeyPair, Error>} generated key pair.
      */
-    self.generateIdentityKeyPair = () => wrappedCrypto.generateKeyPair();
+    this.generateIdentityKeyPair = () => wrappedCrypto.generateKeyPair();
 
     /**
      * Generate a registration ID. Clients should only do this once, at install time.
      *
+     * @method
      * @param {boolean} extendedRange - By default (false), the generated registration
      *                                  ID is sized to require the minimal possible protobuf
      *                                  encoding overhead. Specify true if the caller needs
@@ -47,7 +112,7 @@ function Axolotl(crypto, store) {
      *                                  higher encoding overhead.
      * @return {number} generated registration ID.
      */
-    self.generateRegistrationId = co.wrap(function*(extendedRange) {
+    this.generateRegistrationId = co.wrap(function*(extendedRange) {
         var upperLimit = (extendedRange) ? 0x7ffffffe : 0x3ffc;
         var bytes = yield wrappedCrypto.randomBytes(4);
         var number = new Uint32Array(bytes)[0];
@@ -59,15 +124,16 @@ function Axolotl(crypto, store) {
      * Generate a list of PreKeys.  Clients should do this at install time, and
      * subsequently any time the list of PreKeys stored on the server runs low.
      * <p>
-     * PreKey IDs are shorts, so they will eventually be repeated.  Clients should
+     * PreKey IDs are 16-bit numbers, so they will eventually be repeated.  Clients should
      * store PreKeys in a circular buffer, so that they are repeated as infrequently
      * as possible.
      *
+     * @method
      * @param {number} start - The starting PreKey ID, inclusive.
      * @param {number} count - The number of PreKeys to generate.
-     * @return the list of generated PreKeyRecords.
+     * @return {Promise.<Array.<PreKey>, Error>} the list of generated PreKeyRecords.
      */
-    self.generatePreKeys = co.wrap(function*(start, count) {
+    this.generatePreKeys = co.wrap(function*(start, count) {
         var results = [];
         start--;
         for (var i = 0; i < count; i++) {
@@ -83,9 +149,10 @@ function Axolotl(crypto, store) {
      * Generate the last resort PreKey.  Clients should do this only once, at install
      * time, and durably store it for the length of the install.
      *
-     * @return the generated last resort PreKeyRecord.
+     * @method
+     * @return {Promise.<PreKey, Error>} the generated last resort PreKeyRecord.
      */
-    self.generateLastResortPreKey = co.wrap(function*() {
+    this.generateLastResortPreKey = co.wrap(function*() {
         return {
             id: 0xffffff,
             keyPair: yield wrappedCrypto.generateKeyPair()
@@ -95,12 +162,12 @@ function Axolotl(crypto, store) {
     /**
      * Generate a signed PreKey
      *
+     * @method
      * @param {KeyPair} identityKeyPair - The local client's identity key pair.
      * @param {number} signedPreKeyId - The PreKey id to assign the generated signed PreKey
-     *
-     * @return the generated signed PreKey
+     * @return {SignedPreKey} the generated signed PreKey
      */
-    self.generateSignedPreKey = co.wrap(function*(identityKeyPair, signedPreKeyId) {
+    this.generateSignedPreKey = co.wrap(function*(identityKeyPair, signedPreKeyId) {
         var keyPair = yield wrappedCrypto.generateKeyPair();
         var signature = yield wrappedCrypto.sign(identityKeyPair.private, keyPair.public);
         return {
@@ -110,7 +177,20 @@ function Axolotl(crypto, store) {
         };
     });
 
-    self.encryptMessage = co.wrap(function*(toIdentity, message) {
+    /**
+     * Encrypt a message using the session associated with toIdentity. If no such session exists, it will be created.
+     * <p>
+     * It is safe to call this method multiple times without waiting for the returned promises to be resolved. In this
+     * case, operations may be queued to ensure sessions are in the correct state for each encryption.
+     *
+     * @method
+     * @param {Identity} toIdentity - the identity of the intended recipient
+     * @param {ArrayBuffer} message - the message bytes to be encrypted
+     * @return {Promise.<ArrayBuffer, (InvalidKeyException|UntrustedIdentityException)>} encrypted message bytes if
+     * fulfilled. May be rejected if a fresh session is required and the pre key bundle returned from the server is
+     * invalid.
+     */
+    this.encryptMessage = co.wrap(function*(toIdentity, message) {
         var session;
         var hasSession = yield sessionFactory.hasSessionForIdentity(toIdentity);
         if (hasSession) {
@@ -122,7 +202,19 @@ function Axolotl(crypto, store) {
         return yield session.encryptMessage(message);
     });
 
-    self.decryptWhisperMessage = co.wrap(function*(fromIdentity, message) {
+    /**
+     * Decrypt a WhisperMessage using the session associated with fromIdentity.
+     * <p>
+     * It is safe to call this method multiple times without waiting for the returned promises to be resolved. In this
+     * case, operations may be queued to ensure sessions are in the correct state for each decryption.
+     *
+     * @method
+     * @param {Identity} fromIdentity - the identity of the intended recipient
+     * @param {ArrayBuffer} message - the encrypted message bytes
+     * @return {Promise.<ArrayBuffer, (InvalidMessageException|DuplicateMessageException)>} decrypted message bytes if
+     * fulfilled. May be rejected if a session does not exist, the message is invalid or has been decrypted before.
+     */
+    this.decryptWhisperMessage = co.wrap(function*(fromIdentity, message) {
         var hasSession = yield sessionFactory.hasSessionForIdentity(fromIdentity);
         if (!hasSession) {
             throw new InvalidMessageException("No session for message");
@@ -131,7 +223,19 @@ function Axolotl(crypto, store) {
         return yield session.decryptWhisperMessage(message);
     });
 
-    self.decryptPreKeyWhisperMessage = co.wrap(function*(fromIdentity, message) {
+    /**
+     * Decrypt a PreKeyWhisperMessage using the session associated with fromIdentity.
+     * <p>
+     * It is safe to call this method multiple times without waiting for the returned promises to be resolved. In this
+     * case, operations may be queued to ensure sessions are in the correct state for each decryption.
+     *
+     * @method
+     * @param {Identity} fromIdentity - the identity of the intended recipient
+     * @param {ArrayBuffer} message - the encrypted message bytes
+     * @return {Promise.<ArrayBuffer, (InvalidMessageException|DuplicateMessageException)>} decrypted message bytes if
+     * fulfilled. May be rejected if the message is invalid or has been decrypted before.
+     */
+    this.decryptPreKeyWhisperMessage = co.wrap(function*(fromIdentity, message) {
         var session = yield sessionFactory.createSessionFromPreKeyWhisperMessage(fromIdentity, message);
         return yield session.decryptPreKeyWhisperMessage(message);
     });
