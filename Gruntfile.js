be976b91ae6de757dbad6f8334c79e3e5dccff00
DC <dcposch@dcpos.ch>
2016-01-27 12:01:50 +0100

    Remove Blanket since it doesn't support ES6
    We can probably replace it later with Istanbul, so that we get
    code coverage reports again.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,11 @@
 "use strict";
 
 module.exports = function(grunt) {
-    grunt.loadNpmTasks("grunt-mocha-test"); // For server-side testing
-    grunt.loadNpmTasks("grunt-karma"); // For client-side testing
+    grunt.loadNpmTasks("grunt-mocha-test");
     grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-blanket");
-    grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.loadNpmTasks("grunt-contrib-copy");
-    grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-jscs");
 
     grunt.initConfig({
-        clean: {
-            all: {
-                src: ["coverage/"]
-            }
-        },
         jshint: {
             all: {
                 src: ["Gruntfile.js", "index.js", "src/**/*.js", "test/**/*.js"],
@@ -29,18 +19,6 @@ module.exports = function(grunt) {
                 src: ["index.js", "src/**/*.js", "test/**/*.js"]
             }
         },
-        copy: {
-            coverage: {
-                src: ["test/**"],
-                dest: "coverage/"
-            }
-        },
-        blanket: {
-            all: {
-                src: "src/",
-                dest: "coverage/build/src/"
-            }
-        },
         mochaTest: {
             unitTests: {
                 src: ["test/unit/**/*.js"],
@@ -50,23 +28,12 @@ module.exports = function(grunt) {
                 options: {
                     clearRequireCache: true
                 }
-            },
-            coverage: {
-                src: ["coverage/build/test/unit/**/*.js"],
-                options: {
-                    reporter: "html-cov",
-                    quiet: true,
-                    captureFile: "coverage/index.html",
-                    clearRequireCache: true
-                }
             }
         }
     });
 
-    grunt.registerTask("coverage", ["copy:coverage", "blanket", "mochaTest:coverage"]);
-
     grunt.registerTask("check", ["jshint", "jscs"]);
-    grunt.registerTask("test", ["clean", "check", "mochaTest:unitTests"]);
+    grunt.registerTask("test", ["check", "mochaTest:unitTests"]);
     grunt.registerTask("default", ["test"]);
     grunt.registerTask("integration-test", ["mochaTest:integrationTests"]);
 };
